cacea90b00dd1f7875e68b92395f0987f57c1e7e
Go <gosha.levinson@gmail.com>
2021-04-11 16:08:42 +0300

    Initial commit. Empty classes created for the first five problems.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlgoCasts_Solution
+{
+    static class StringReversal
+    {
+
+    }
+    static class Palindromes
+    {
+
+    }
+    static class IntegerReversal
+    {
+
+    }
+    static class MaxChars
+    {
+
+    }
+    static class FizzBuzz
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
