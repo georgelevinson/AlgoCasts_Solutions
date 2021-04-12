bad45a623609efeffa550a48475f662f21eb3ec2
Go <gosha.levinson@gmail.com>
2021-04-12 13:33:03 +0300

    StringReversal class: ReverseIterative(), ReverseRecursive() and ReverseUsingArrayMethod() methods implemented and tested.  Palindromes class: IsPalindromeUsingReverse() and IsPalindromeCharComparison() methods implemented and tested.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AlgoCasts_Solution
 {
@@ -13,9 +15,46 @@ namespace AlgoCasts_Solution
             }
             return rev;
         }
+        public static string ReverseRecursive(string str, string rev = "", int i = 1)
+        {
+            if (i > str.Length)
+                return rev;
+            rev += str[str.Length - i];
+            return ReverseRecursive(str, rev, i + 1);
+        }
+        public static string ReverseUsingArrayMethod(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
     }
     static class Palindromes
     {
+        public static bool IsPalindromeUsingReverse(string str)
+        {
+            if (StringReversal.ReverseUsingArrayMethod(str) == str)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsPalindromCharComparison(string str)
+        {
+            static IEnumerable<bool> CharEqulity(string str)
+            {
+                for (int i = 0; i <= str.Length / 2; i++)
+                {
+                    yield return str[i] == str[str.Length - (1 + i)];
+                }
+            }
+
+            List<bool> charEq = CharEqulity(str).ToList();
+
+            if (!charEq.Contains(false))
+                return true;
+            else
+                return false;
+        }
     }
     static class IntegerReversal
     {
@@ -34,7 +73,7 @@ namespace AlgoCasts_Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(StringReversal.ReverseIterative("sck my ass"));
+            Console.WriteLine(Palindromes.IsPalindromCharComparison("aasssaa  "));
         }
     }
 }
