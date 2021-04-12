6fb52950d98a36ae6dff9233622ab541c0615e6a
Go <gosha.levinson@gmail.com>
2021-04-12 15:16:18 +0300

    IntegerReversal class: Reverse() method implemented and tested. MaxChars class: FindMaxChars() method implemented and tested.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace AlgoCasts_Solution
 {
     static class StringReversal
     {
+        // --- Directions
+        // Given a string, return a new string with the reversed
+        // order of characters
+        // --- Examples
+        //   reverse('apple') === 'leppa'
+        //   reverse('hello') === 'olleh'
+        //   reverse('Greetings!') === '!sgniteerG'
+
         public static string ReverseIterative(string str)
         {
             string rev = "";
@@ -31,6 +40,15 @@ namespace AlgoCasts_Solution
     }
     static class Palindromes
     {
+        // --- Directions
+        // Given a string, return true if the string is a palindrome
+        // or false if it is not.  Palindromes are strings that
+        // form the same word if it is reversed. *Do* include spaces
+        // and punctuation in determining if the string is a palindrome.
+        // --- Examples:
+        //   palindrome("abba") === true
+        //   palindrome("abcdefg") === false
+
         public static bool IsPalindromeUsingReverse(string str)
         {
             if (StringReversal.ReverseUsingArrayMethod(str) == str)
@@ -58,22 +76,91 @@ namespace AlgoCasts_Solution
     }
     static class IntegerReversal
     {
+        // --- Directions
+        // Given an integer, return an integer that is the reverse
+        // ordering of numbers.
+        // --- Examples
+        //   reverseInt(15) === 51
+        //   reverseInt(981) === 189
+        //   reverseInt(500) === 5
+        //   reverseInt(-15) === -51
+        //   reverseInt(-90) === -9
 
+        public static int Reverse(int num)
+        {
+            string str = num.ToString().Trim('-');
+            string revStr = StringReversal.ReverseUsingArrayMethod(str);
+            return Int32.Parse(revStr) * Math.Sign(num);
+        }
     }
     static class MaxChars
     {
+        // --- Directions
+        // Given a string, return the character that is most
+        // commonly used in the string.
+        // --- Examples
+        // maxChar("abcccccccd") === "c"
+        // maxChar("apple 1231111") === "1"
 
+        public static char FindMaxChars(string str)
+        {
+            Dictionary<char, int> charAppearsTimes = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (charAppearsTimes.ContainsKey(c))
+                    charAppearsTimes[c]++;
+                else
+                    charAppearsTimes.Add(c, 1);
+            }
+
+            var keys = charAppearsTimes.Keys.ToImmutableList();
+            var values = charAppearsTimes.Values;
+            int index = 0;
+            int maxIndex = 0;
+            int maxValue = 0;
+
+            foreach (int value in values)
+            {
+                if (value > maxValue)
+                    maxIndex = index;
+                    maxValue = value;
+
+                index++;
+            }
+
+            return keys[maxIndex];
+        }
     }
     static class FizzBuzz
     {
 
     }
+    static class ArrayChunking
+    {
 
+    }
+    static class Anagrams
+    {
+
+    }
+    static class SentenceCapitalization
+    {
+
+    }
+    static class PrintingSteps
+    {
+
+    }
+    static class Pyramid
+    {
+
+    }
     static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Palindromes.IsPalindromCharComparison("aasssaa  "));
+            Console.WriteLine(MaxChars.FindMaxChars("abbbbcccccdddd")); ;
         }
     }
 }
